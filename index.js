ad35923d0ccf3638053e23fc8a1be11e30cc63d9
Sarthak gupta <guptasarthak03@gmail.com>
2022-09-01 22:42:49 +0530

    Style: responsiveness

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const billInputDOM = document.querySelector('#bill-amount');
 const depositInputDOM = document.querySelector('#cash-deposit');
 const outputBoxDOM = document.querySelector('.output-container');
+const tableCaptionDOM = document.querySelector('caption');
 const calculateButton = document.querySelector('.btn-calculate');
-const errorMsgDOM = document.querySelector('.error-msg');
+const messageDOM = document.querySelector('.error-msg');
 const noteListDOM = document.querySelectorAll('.currency-note');
 
-const currencyList = [2000, 500, 100, 50, 10, 5, 2, 1];
+const currencyList = [2000, 500, 100, 50, 10, 5, 1];
 
 calculateButton.addEventListener('click', () => {
+  // value setup for first time and next time
   const bill = Number(billInputDOM.value);
   const deposit = Number(depositInputDOM.value);
+  let showResult = false;
 
   // validation: empty, negative or 0. (if )
   const isInputValidated = validateInput(bill, deposit);
@@ -17,36 +20,40 @@ calculateButton.addEventListener('click', () => {
   // logic
   if (isInputValidated) {
     const change = deposit - bill;
-    calculateChange(change);
-    outputBoxDOM.style.display = 'block';
+    showResult = calculateChange(change);
   }
+
+  // display results (conditional)
+  outputBoxDOM.style.display = showResult ? 'block' : 'none';
 });
 
-const showError = msg => {
-  errorMsgDOM.style.display = 'block';
-  errorMsgDOM.innerText = `Error: ${msg}`;
+const showMessage = msg => {
+  messageDOM.style.display = 'block';
+  messageDOM.innerText = `${msg}`;
 };
 
 const validateInput = (bill, deposit) => {
   // validation: empty, negative or 0.
   switch (true) {
     case !bill:
-      showError('Bill Amount cannot be empty');
+      showMessage('Error: Bill Amount cannot be empty');
       return false;
     case bill <= 0:
-      showError('Bill Amount cannot be zero or negative');
+      showMessage('Error: Bill Amount cannot be zero or negative');
       return false;
     case !deposit:
-      showError('Cash Deposit cannot be empty');
+      showMessage('Error: Cash Deposit cannot be empty');
       return false;
     case deposit <= 0:
-      showError('Cash Deposit cannot be zero or negative');
+      showMessage('Error: Cash Deposit cannot be zero or negative');
       return false;
     case bill > deposit:
-      showError('Cash Deposit must atleast be equal to the Bill Amount');
+      showMessage(
+        'Error: Cash Deposit must atleast be equal to the Bill Amount'
+      );
       return false;
     default:
-      errorMsgDOM.style.display = 'none';
+      messageDOM.style.display = 'none';
   }
   return true;
 };
@@ -54,25 +61,33 @@ const validateInput = (bill, deposit) => {
 const calculateChange = change => {
   // if no change then return
   if (change === 0) {
-    console.log('no cash to be returned');
-    return;
+    showMessage('No change to be returned');
+    return false;
   }
 
+  // show total amount in caption
+  console.log(tableCaptionDOM.innerText);
+  tableCaptionDOM.innerText = `Change to be returned: ₹${change} `;
+
+  // Calculate change in currency
   for (let i = 0; i < currencyList.length; i++) {
+    debugger;
     let notes = 0;
     let currency = currencyList[i];
     let check = change % currency; // remainder or same
 
-    // case where currency note is bigger than the change
+    // case where currency note is  &enspbigger than the change
     if (check == change) {
       continue;
     }
 
     if (check != change || check == 0) {
       notes = Math.trunc(change / currency);
-      noteListDOM[i].innerText = notes;
+      noteListDOM[i].innerText = notes; // populate no of currency notes in table
 
       change = check;
     }
   }
+
+  return true;
 };
